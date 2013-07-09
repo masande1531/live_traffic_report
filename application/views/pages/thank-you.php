a810b221eddc479ed6214d1116c02e5fd9bdf959
masande1531 <masande@globalimsa.com>
2013-07-09 16:03:45 +0200

    Create thank-you.php

---
+++ application/views/pages/thank-you.php
@@ -0,0 +1,23 @@
+<!-- start page -->
+<div id="page">
+<!-- start sidebar -->
+  <div id="sidebar">
+		
+				<? $this->load->view('pages/side_bar.php'); ?>
+			
+	</div>
+	<!-- end sidebar -->
+    <div id="content">
+        <div class="post">
+            <div class="title">
+
+                <br/><small><a href="http://localhost/ci_traffic/index.php/view_report">View Reports</a> | <a href="http://localhost/ci_traffic/index.php/report">Report</a> | <a href="http://localhost/ci_traffic/">Live Traffic</a></small><br/><br/>
+            </div>
+            <div class="entry">
+<h2>Thanks for registering!</h2>
+Your confirmation email is on its way. Please click the link in the 
+email to complete the registration.
+</div>
+        </div>
+    </div>
+</div>
