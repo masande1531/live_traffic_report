056287260b6bb97399658600c18c09a0e0e215f1
masande1531 <masande@globalimsa.com>
2013-07-09 16:01:30 +0200

    Create reset_message.php

---
+++ application/views/pages/reset_message.php
@@ -0,0 +1,23 @@
+<div id="page">
+<!-- start sidebar -->
+  <div id="sidebar">
+		
+				<? $this->load->view('pages/side_bar.php'); ?>
+			
+	</div>
+	<!-- end sidebar -->
+    <div id="content">
+        <div class="post">
+            <div class="title">
+
+                <br/><small><a href="http://localhost/ci_traffic/index.php/view_report">View Reports</a> | <a href="http://localhost/ci_traffic/index.php/report">Report</a> | <a href="http://localhost/ci_traffic/">Live Traffic</a></small><br/><br/>
+            </div>
+            <div class="entry">
+                <br style="clear: both;" /><br style="clear: both;" />
+<h2>Thanks for reseting your password!</h2>
+Your confirmation email is on its way. Please click the link in the 
+email to complete the password reseting.
+</div>
+        </div>
+    </div>
+</div>
