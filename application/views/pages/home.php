13375e5f2ceac578d8ce6c440e3923e3dda2762a
masande1531 <masande@globalimsa.com>
2013-07-09 15:56:38 +0200

    Create home.php

---
+++ application/views/pages/home.php
@@ -0,0 +1,41 @@
+
+<!-- start page -->
+<div id="page">
+  <!-- start sidebar -->
+	<div id="sidebar">
+		
+				<? $this->load->view('pages/side_bar.php'); ?>
+			
+	</div>
+	<!-- end sidebar -->
+	<!-- start content -->
+	<div id="content">
+		<div class="post">
+			<div class="title">
+				<br/><small><a href="<? echo base_url(); ?>index.php/view_report">View Reports</a> | <a href="<? echo base_url(); ?>index.php/report">Report</a> | <a href="<? echo base_url(); ?>">Live Traffic</a></small><br/><br/>
+			</div>
+			<div class="entry">
+                         <img src="images/masa.gif" width="750" height="400" style="opacity:0.9;" class="jms-wrapper"/>
+                         <div id="caption">
+                             <h3>If you want to know what's happening on our national roads don't hesitate to view live traffic</h3>
+                         </div>
+                            
+                          <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <link rel="stylesheet" href="http://jqueryui.com/jquery-wp-content/themes/jqueryui.com/style.css" />
+  <script>
+
+  </script>
+                           <div id="dialog1"><a class="jms-link" href="#" id="openerd">view map</a></div>
+                        </div>
+                    
+
+		</div>
+	
+	</div>
+	<!-- end content -->
+	<br style="clear: both;" />
+</div>
+<!-- end page -->
+<!-- start footer -->
