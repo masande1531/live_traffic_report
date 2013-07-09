7a8ed5edcbf22f6d427608b286e301055c566b89
masande1531 <masande@globalimsa.com>
2013-07-09 15:54:24 +0200

    Create about.php

---
+++ application/views/pages/about.php
@@ -0,0 +1,39 @@
+
+<!-- start page -->
+<div id="page">
+  <!-- start sidebar -->
+	<div id="sidebar">
+		<? $this->load->view('pages/side_bar.php'); ?>
+	</div>
+	<!-- end sidebar -->
+	<!-- start content -->
+	<div id="content">
+		<div class="post">
+			<div class="title">
+				
+				<br/><small><a href="http://localhost/ci_traffic/index.php/view_report">View Reports</a> | <a href="http://localhost/ci_traffic/index.php/report">Report</a> | <a href="http://localhost/ci_traffic/">Live Traffic</a></small><br/><br/>
+			</div>
+			<div class="entry">
+                            <h2>About Traffic Reports</h2>
+                            <p>
+                                This web application gives  details off activities taking place in public roads. 
+                                The web application was developed by Masande try and solve traffic solve traffic congestion during pick up hours.
+                            </p>
+                            <p>
+                                This web application gives  details off activities taking place in public roads. 
+                                The web application was developed by Masande try and solve traffic solve traffic congestion during pick up hours.
+                            </p>
+                            <p>
+                                This web application gives  details off activities taking place in public roads. 
+                                The web application was developed by Masande try and solve traffic solve traffic congestion during pick up hours.
+                            </p>
+                        </div>
+			
+		</div>
+	
+	</div>
+	<!-- end content -->
+	<br style="clear: both;" />
+</div>
+<!-- end page -->
+<!-- start footer -->
