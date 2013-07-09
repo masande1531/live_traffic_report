07df5c49e3f6679fcda65d713e51398b0512b0fc
masande1531 <masande@globalimsa.com>
2013-07-09 15:59:36 +0200

    Create reset_complete.php

---
+++ application/views/pages/reset_complete.php
@@ -0,0 +1,25 @@
+<div id="page">
+<!-- start sidebar -->
+  <div id="sidebar">
+		
+				<? $this->load->view('pages/side_bar.php'); ?>
+			
+	</div>
+	<!-- end sidebar -->
+    <div id="content">
+        <div class="post">
+            <div class="title">
+
+                <br/><small><a href="http://localhost/ci_traffic/index.php/view_report">View Reports</a> | <a href="http://localhost/ci_traffic/index.php/report">Report</a> | <a href="http://localhost/ci_traffic/">Live Traffic</a></small><br/><br/>
+            </div>
+            <div class="entry">
+                <br style="clear: both;" /><br style="clear: both;" />
+<h2>Congratulations!</h2>
+<p>Your new password has been sent to your email.</p>  
+<p>
+<a href='http://localhost/ci_traffic/index.php/page/login'>Click here to login</a>
+            </div>
+        </div>
+    </div>
+</div>
+    
