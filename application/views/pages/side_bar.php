a83d584806ec69d4960d9b855fc107d212b34b6c
masande1531 <masande@globalimsa.com>
2013-07-09 16:02:01 +0200

    Create side_bar.php

---
+++ application/views/pages/side_bar.php
@@ -0,0 +1,13 @@
+<ul>
+
+
+            <li>
+               
+                <ul>
+                   <a class="twitter-timeline" href="https://twitter.com/satrafficupdate" data-widget-id="324121200571850752">Tweets by @satrafficupdate</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+                </ul>
+            </li>
+
+        </ul>
