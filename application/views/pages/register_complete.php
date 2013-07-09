a8dbeca0496e2274c03a3b165a7f6b0a480667a6
masande1531 <masande@globalimsa.com>
2013-07-09 15:58:26 +0200

    Create register_complete.php

---
+++ application/views/pages/register_complete.php
@@ -0,0 +1,27 @@
+<div id="page">
+<!-- start sidebar -->
+  <div id="sidebar">
+		
+				<? $this->load->view('pages/side_bar.php'); ?>
+			
+	</div>
+	<!-- end sidebar -->
+    <div id="content">
+        <div class="post">
+            <div class="title">
+
+                <br/><small><a href="http://localhost/ci_traffic/index.php/view_report">View Reports</a> | <a href="http://localhost/ci_traffic/index.php/report">Report</a> | <a href="http://localhost/ci_traffic/">Live Traffic</a></small><br/><br/>
+            </div>
+            <div class="entry">
+                <br style="clear: both;" /><br style="clear: both;" />
+                
+                
+<h2>Thanks for registering!</h2>
+Your registration is now complete.
+
+<p>
+<a href='http://localhost/ci_traffic/index.php/page/login'>Click here to login</a>
+</div>
+        </div>
+    </div>
+</div>
